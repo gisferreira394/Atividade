1e8d054afab4b6e2c5fb34a51d0b0c930c3ec4cf
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-10-18 21:03:48 -0300

    Cáculo de Média

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log("Olá, estou funcionando")
+// console.log(29*9-6/2)
+// console.log(39+20958/6)
+// // console.log(40+3)
+
+let nome = "Gi"
+let nota1 = 9
+let nota2 = 7
+let nota3 = 4
+let nota4 = 2
+let media = (nota1 + nota2 + nota3 + nota4)/4
+let mediaFixada = media.toFixed(1) 
+
+console.log("Olá " + nome)
+console.log(" Sua média foi: " + mediaFixada)
